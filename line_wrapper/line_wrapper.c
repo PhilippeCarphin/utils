96063f77e5f3a2518c70fa710b351b638330611a
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-26 23:53:52 -0400

    gotta do it

---
+++ line_wrapper/line_wrapper.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int isDelim(char c){
+   switch(c){
+      case '\0':
+      case '\t':
+      case ' ' :
+         return 1;
+         break; /* As a matter of style, put the 'break' anyway even if there is a return above it.*/
+      default:
+         return 0;
+   }
+}
+
+int printLine(const char * start, const char * end){
+   const char * p = start;
+   while ( p <= end ) putchar(*p++);
+   putchar('\n');
+   return 0;
+}
+
+int main ( int argc , char ** argv ) {
+
+   if( argc <= 2 ) exit(1);
+
+   char * start = argv[1];
+   char * lastDelim = argv[1];
+   char * current = argv[1];
+   int wrapLength = atoi(argv[2]);
+   char c = *current
+
+   int chars = 1;
+
+   /* Go to 80 chars */
+   while(c != 0){
+      lastDelim = sart;
+      while((c = *current++) != 0 && chars++ <= wrapLength ){
+         if( isDelim(c) )
+            lastDelim = current - 1;
+      }
+      printLine(start,lastDelim);
+      start = current;
+      chars = 1;
+   }
+
+
+
+   return 0;
+}
