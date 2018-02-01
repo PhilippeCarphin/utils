f2b6068468eaea73e1f2df54a6dfac2c6e9f59a7
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-01-31 21:41:42 -0500

    Added options to todo.sh

--- todo.sh
+++ todo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-TODO_FILE=~/Dropbox/todo.txt
-if [[ "$1" == "" ]] ; then
-	cat $TODO_FILE
-else
-	echo "$(date): $1" >> $TODO_FILE
-fi
 
+TODO_FILE=~/Dropbox/todo.txt
+if [[ "$1" == -o ]] || [[ "$1" == --open ]] ; then
+	vim $TODO_FILE
+else
+	if [[ "$1" == "" ]] ; then
+		echo "cat $TODO_FILE (use --open to open file in vim)"
+		cat $TODO_FILE
+	else
+		echo "$(date): $1" >> $TODO_FILE
+	fi
+fi
