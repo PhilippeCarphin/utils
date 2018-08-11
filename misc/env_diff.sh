ea8006295a3ee2b4283a1a505d9f0e33312fc862
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-10 23:38:14 -0400

    Tidied up helper scripts for environment analyser

--- misc/env_diff.sh
+++ misc/env_diff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 env_before_file=/tmp/$(whoami)_env_before.txt
 env_after_file=/tmp/$(whoami)_env_after.txt
-python3 ~/Documents/GitHub/utils/bin/env_analyser.py dump > $env_before_file
+env_analyser dump > $env_before_file
 eval $@
-python3 ~/Documents/GitHub/utils/bin/env_analyser.py dump > $env_after_file
+env_analyser dump > $env_after_file
 
-python3 ~/Documents/GitHub/utils/bin/env_analyser.py compare $env_before_file $env_after_file
+env_analyser compare $env_before_file $env_after_file
