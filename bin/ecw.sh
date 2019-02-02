051fc7063e62bec556f004a79ad45d301ef93932
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-01 22:52:23 -0500

    Upgraded ecw

--- bin/ecw.sh
+++ bin/ecw.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-filepath=$(which $1); shift
+argument_found=false
+for f in "$@" ; do
+    if [[ $f == -* ]] ; then
+        args=$"$args $f"
+    else
+        if ! which $f > /dev/null 2>&1 ; then
+            which $f
+            exit 1;
+        fi
+        args="$args $(which $f)"
+        argument_found=true
+    fi
+done
+
+if ! $argument_found ; then
+    echo "ERROR : You must supply the name of a program to search for"
+fi
+
 if emacsclient -a false -e 't' >/dev/null ; then
-    ec $filepath $@
+    emacsclient -t $args
 else
     vim $filepath $@
 fi
