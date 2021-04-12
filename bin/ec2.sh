58bb9b80ce89795e267f1803454079adeeb46934
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-12 11:10:20 -0400

    Cleanup utils

--- bin/ec2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if ! emacsclient -c -e '(gtd-dashboard)' 2>/dev/null 1>/dev/null ; then
-   cd ~/Dropbox/Notes/gtd/
-   vim *.org -p
-fi
--- bin/edit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if emacsclient -a false -e 't' >/dev/null ; then
-    ec -ct $@
-else
-    vim $@
-fi
