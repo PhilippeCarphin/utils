39e2ca58ff1b7d7f76ba40400f1f7895f9ae8cf2
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-08-10 18:27:18 -0400

    Upgrade echo-server

--- libexec/philutils/echo-server.py
+++ libexec/philutils/echo-server.py
@@ -5,6 +5,12 @@ from pprint import pprint
 import json
 import argparse
 import sys
+import multipart
+from io import BytesIO
+import subprocess
+
+check_jq = subprocess.run(['which', 'jq'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+use_jq = (check_jq.returncode == 0)
 
 DESCRIPTION="Launch a server that prints out POST requests it receives"
 
@@ -17,19 +23,38 @@ def get_args():
 
 class MyServer(http.server.BaseHTTPRequestHandler):
     def generic_handler(self):
-        print(f"Headers\n=======\n\n\033[36m{str(self.headers).strip()}\033[0m\n")
+        print(self.requestline)
+        print(f"Headers\n=======\n\033[36m")
+        for k,v in self.headers.items():
+            print(f"\033[36m'{k}': '{v}'\033[0m")
         if 'Content-Length' in self.headers:
             content_length = int(self.headers['Content-Length'])
             request_body = self.rfile.read(content_length).decode('utf-8')
-            if 'Content-Type' in self.headers and self.headers['Content-Type'] == 'application/json':
-                request_dict = json.loads(request_body)
-                print("JSON body\n==============\n")
-                print("\033[1;32m", end="")
-                pprint(request_dict)
-                print("\033[0m", end="")
-                print()
+            if self.headers['Content-Type'] == 'application/json':
+                print("\nJSON body\n==============\n")
+                if use_jq:
+                    p = subprocess.Popen(['jq'], stdin=subprocess.PIPE, universal_newlines=True)
+                    p.stdin.write(request_body)
+                    p.stdin.close()
+                    p.wait()
+                else:
+                    request_dict = json.loads(request_body)
+                    print("\033[1;32m", end='')
+                    pprint(request_dict)
+                print("\033[0m")
+            elif self.headers['Content-Type'].startswith('multipart/form-data'):
+                print("\nForm data\n=========\n")
+                s = request_body.split("\r")[0][2:]
+                p = multipart.MultipartParser(BytesIO(multipart.to_bytes(request_body)),s)
+                parts = p.parts()
+                while True:
+                    try:
+                        item = parts.pop()
+                        print(f"\033[1;33mForm item: '{item.name}': '{item.value}'\033[0m")
+                    except IndexError:
+                        break
             else:
-                print(f"Request body\n============\n\n\033[1;33m{request_body}\033[0m\n")
+                print(f"\nRequest body\n============\n\n\033[1;33m{request_body}\033[0m\n")
 
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
