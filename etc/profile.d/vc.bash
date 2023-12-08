1eef2c37f8ec4942ed9ee3f4078abe76714c94c8
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-12-08 17:22:20 -0500

    Custom completion for vc and whence
    'complete -c vc' was not getting the sourceable files so I wrote a
    completion function for vc and whence.

--- etc/profile.d/vc.bash
+++ etc/profile.d/vc.bash
@@ -40,7 +40,71 @@ vc(){
         return
     fi
 }
+
+# Sourceable here means
+# - not executable `compgen -c` will pick up executables from PATH anyway
+#   and it reduces the number of candidates to run the `file` command on.
+# - ASCII text Note: running `file` is the most time consuming step so
+#   it should be done only on the candidates that have passed all of the
+#   other checks.
+_vc_add_path_sourceable(){
+    _vc_add_path_sourceable_shell_wildcard
+}
+#
+# Doesn't work if there are two colons together in PATH, this causes
+# an empty string to be passed to find
+#
+_vc_add_path_sourceable_find(){
+    local IFS=$': \n'
+    for f in $(find -L ${PATH} -maxdepth 1 -name "${cur}*" ! -executable -readable) ; do
+        if ! [[ "$(file -L ${f})" == *ASCII* ]] ; then
+            continue
         fi
+        COMPREPLY+=(${f##*/})
+    done
+}
+_vc_add_path_sourceable_shell_wildcard(){
+    local IFS=:
+    for p in ${PATH} ; do {
+        local IFS=$'\n'
+        for f in ${p}/* ; do
+            if ! [[ ${f} == ${p}/${cur}* ]] ; then
+                continue
+            fi
+            if [[ -x ${f} ]] ; then
+                continue
+            fi
+            if ! [[ "$(file -L ${f})" == *ASCII* ]] ; then
+                continue
+            fi
+            COMPREPLY+=(${f##*/})
+        done
+    } done
+}
+
+
+# Two different methods.  One getting candidates using find and one using shell
+# wildcard expansion.  Both methods perform similarily however it is good to
+# note that when XTRACE is on, the wildcard one is much slower than the
+# find one because there is simply more tracing going on.  However, the find
+# one seems to use significantly more system time and the shell one seems
+# to take much more user time.  Both end up with similar real time.
+_vc_test_add_path_sourceable(){
+    local cur=s
+    COMPREPLY=()
+    time _vc_add_path_sourceable_find
+    echo "${COMPREPLY[*]}"
+    echo "========================================"
+    COMPREPLY=()
+    time _vc_add_path_sourceable_shell_wildcard
+    echo "${COMPREPLY[*]}"
+}
+
+_vc(){
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=($(compgen -c -- "${cur}"))
+    if shopt -q sourcepath ; then
+        _vc_add_path_sourceable
     fi
 }
 
@@ -120,6 +184,6 @@ whence()(
 
 )
 
-complete -c vc whence
+complete -F _vc vc whence
 complete -F _open_shell_function open_shell_function
 
