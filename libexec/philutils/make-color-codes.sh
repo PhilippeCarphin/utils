4caa8cd6c03e5bbfcaa04e90d6413ba21d208aba
Philippe Carphin <philippe.carphin@polymtl.ca>
2023-12-08 20:13:17 -0500

    Add grayscale conversion of RGB colors

--- libexec/philutils/make-color-codes.sh
+++ libexec/philutils/make-color-codes.sh
@@ -72,7 +72,6 @@ zero-pad-to-3-digits () {
 }
 
 print_code(){
-
     local code=$1
     if (( code <= 255 )) && (( 232 <= code)) ; then
         if (( 248 <= code )) && (( code <= 255 )) ; then
@@ -80,14 +79,47 @@ print_code(){
         else
             fg=$'\033[38;5;15m'
         fi
+        printf "\033[48;5;${code}m${fg}$(zero-pad-to-3-digits ${code} )\033[0m"
+        # printf "\033[48;5;${code}m${fg}   \033[0m"
     else
         if (( 24 <= ((code - 16) % 36) )) ; then
             fg=$'\033[38;5;0m'
         else
             fg=$'\033[38;5;15m'
         fi
+        if [[ -n ${grayscale} ]] ; then
+            # Testing ANSI colorcube to ANSI grayscale [232,255]
+            gcode=$(ansi_to_grayscale ${code})
+            printf "\033[48;5;${gcode}m${fg}   \033[0m"
+        else
+            printf "\033[48;5;${code}m${fg}$(zero-pad-to-3-digits ${code} )\033[0m"
+        fi
+
     fi
-    printf "\033[48;5;${code}m${fg}$(zero-pad-to-3-digits ${code} )\033[0m"
+}
+
+map=(0 95 135 175 215 255)
+# Code from [16,231] (code is 16 + 36r + 6g + b where r,g,b in [0,5])
+# And [0,5] --map--> [0,255]
+# [0,255]x[0,255]x[0,255] ---> [0,255] with 0.3*R + 0.59*G + 0.11*B
+# [0,255] ---> [232,255] (range of grayscale ansi codes)
+ansi_to_grayscale(){
+    local ansi=$1
+    if (( ansi < 16 )) ||((ansi > 231)) ; then
+        return 1
+    fi
+
+    # Extract r,g,b from ansi = 16 + 36r + 6g + b (r,g,b in [0,5])
+    ansi=$((ansi-16))
+    local b=$((ansi % 6))
+    local g=$(( (ansi/6) % 6 ))
+    local r=$(( (ansi/36) ))
+
+    # Use gray = 0.3R + 0.59G + 0.11B (R,G,B in [0,255]
+    local gray=$(( (30*map[r] + 59*map[g] + 11*map[b]) / 100 ))
+    # Map [0,255] to [232,255]
+    local code=$((232 + (gray * (255-232))/255))
+    echo "${code}"
 }
 
 ################################################################################
