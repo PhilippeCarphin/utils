655a21a52779bb235a0bec81eb4fb7a146dfa415
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-12-13 11:50:22 -0500

    Slight refactor, add query params, tighten output
    The method name is passed to the generic_handler to reduce code
    repetition.
    
    Print query parameters.
    
    Reduce the whitespace within the output for a single request to make the
    output of different requests more easily distinguishable.

--- libexec/philutils/echo-server.py
+++ libexec/philutils/echo-server.py
@@ -8,6 +8,7 @@ import sys
 import multipart
 from io import BytesIO
 import subprocess
+import urllib
 
 check_jq = subprocess.run(['which', 'jq'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 use_jq = (check_jq.returncode == 0)
@@ -22,16 +23,24 @@ def get_args():
     return p.parse_args()
 
 class MyServer(http.server.BaseHTTPRequestHandler):
-    def generic_handler(self):
+    def generic_handler(self,method):
+        qp = urllib.parse.urlparse(self.path)
+        path, query = qp.path, qp.query
+        print(f"\n\033[1;4mIncoming \033[34m{method}\033[39m request on path = \033[35m{path}\033[0m")
         print(self.requestline)
-        print(f"Headers\n=======\n\033[36m")
+        if query:
+            print(f"Query Parameters\n================\033[35m")
+            for k,v in map(lambda kv: kv.split("="), query.split('&')): # What's the point of urlparse if I have to do this myself?
+                print(f"{k}: {v}")
+            print("\033[0m", end='')
+        print(f"Headers\n=======\033[36m")
         for k,v in self.headers.items():
             print(f"\033[36m'{k}': '{v}'\033[0m")
         if 'Content-Length' in self.headers:
             content_length = int(self.headers['Content-Length'])
             request_body = self.rfile.read(content_length).decode('utf-8')
             if self.headers['Content-Type'] == 'application/json':
-                print("\nJSON body\n==============\n")
+                print("JSON body\n==============")
                 if use_jq:
                     p = subprocess.Popen(['jq'], stdin=subprocess.PIPE, universal_newlines=True)
                     p.stdin.write(request_body)
@@ -41,9 +50,9 @@ class MyServer(http.server.BaseHTTPRequestHandler):
                     request_dict = json.loads(request_body)
                     print("\033[1;32m", end='')
                     pprint(request_dict)
-                print("\033[0m")
+                print("\033[0m", end='')
             elif self.headers['Content-Type'].startswith('multipart/form-data'):
-                print("\nForm data\n=========\n")
+                print("Form data\n=========")
                 s = request_body.split("\r")[0][2:]
                 p = multipart.MultipartParser(BytesIO(multipart.to_bytes(request_body)),s)
                 parts = p.parts()
@@ -54,25 +63,22 @@ class MyServer(http.server.BaseHTTPRequestHandler):
                     except IndexError:
                         break
             else:
-                print(f"\nRequest body\n============\n\n\033[1;33m{request_body}\033[0m\n")
+                print(f"\nRequest body\n============\n\033[1;33m{request_body}\033[0m")
 
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
+        # json.dump({"message": "Hello World"}, self.wfile)
         self.wfile.write(bytes(json.dumps({"message": "Hello World"},indent='    ') + '\n', 'utf-8'))
         self.send_response(200)
 
     def do_GET(self):
-        print(f"Incoming \033[1;34mGET\033[0m request on path = \033[35m{self.path}\033[0m")
-        self.generic_handler()
+        self.generic_handler("GET")
     def do_POST(self):
-        print(f"Incoming \033[1;34mPOST\033[0m request on path = \033[35m{self.path}\033[0m")
-        self.generic_handler()
+        self.generic_handler("POST")
     def do_DELETE(self):
-        print(f"Incoming \033[1;34mDELETE\033[0m request on path = \033[35m{self.path}\033[0m")
-        self.generic_handler()
+        self.generic_handler("DELETE")
     def do_PUT(self):
-        print(f"Incoming \033[1;34mPUT\033[0m request on path = \033[35m{self.path}\033[0m")
-        self.generic_handler()
+        self.generic_handler("PUT")
 
 args = get_args()
 if args.curl_notes:
@@ -98,4 +104,7 @@ server = http.server.HTTPServer((args.host, args.port), MyServer)
 
 print(f"Server listening on address : \033[1;33m{args.host}\033[0m, port \033[1;34m{args.port}\033[0m")
 
-server.serve_forever()
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    quit(130)
