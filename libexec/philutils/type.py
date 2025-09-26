a9b87a253a4028765aca7e96b687b9ad24bae06b
Philippe Carphin <phil103@hotmail.com>
2025-09-25 21:32:57 -0400

    type requires pygments since if I don't have pygments I want it to fail

--- libexec/philutils/type.py
+++ libexec/philutils/type.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 
 code = sys.stdin.read()
+
 if code == "":
     sys.exit(1)
 
@@ -14,28 +15,28 @@ try:
     import pygments.token
 except ImportError as e:
     print(f"INFO: pygment imports failed: {e}")
+    print(f"INFO: Printing code withotu color")
     print(code)
     sys.exit(0)
 
-if 'pygments' in sys.modules:
-    def get_formatter():
-        styles = list(pygments.styles.get_all_styles())
-        style = 'default'
-        # Solarized light is for light background but I tried it on dark background
-        # and it looks great
-        potential_styles = ['solarized-light', 'vim', 'monokai', 'arduino',
-                            'emacs', 'native', 'lovelace', 'paraiso-dark',
-                            'rainbow_dash', 'rrt', 'perldoc', 'solarized-dark',
-                            'sas', 'stata-dark', 'dracula', 'colorful']
-        for s in potential_styles:
-            if s in styles:
-                style = s
-                break
-        return pygments.formatters.Terminal256Formatter(style=style)
+def get_formatter():
+    styles = list(pygments.styles.get_all_styles())
+    style = 'default'
+    # Solarized light is for light background but I tried it on dark background
+    # and it looks great
+    potential_styles = ['solarized-light', 'vim', 'monokai', 'arduino',
+                        'emacs', 'native', 'lovelace', 'paraiso-dark',
+                        'rainbow_dash', 'rrt', 'perldoc', 'solarized-dark',
+                        'sas', 'stata-dark', 'dracula', 'colorful']
+    for s in potential_styles:
+        if s in styles:
+            style = s
+            break
+    return pygments.formatters.Terminal256Formatter(style=style)
 
-    lexer = pygments.lexers.shell.BashLexer()
-    fmt = get_formatter()
-    def highlight(code):
-        return pygments.highlight(code, lexer=lexer, formatter=fmt)
+lexer = pygments.lexers.shell.BashLexer()
+
+def highlight(code):
+    return pygments.highlight(code, lexer=lexer, formatter=get_formatter())
 
 print(highlight(code))
