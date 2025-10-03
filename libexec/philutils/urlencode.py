7b0a3b291d47a58b2a13a18832811ba938a828c5
Philippe Carphin <phil103@hotmail.com>
2025-10-02 21:02:02 -0400

    Improve urlencode
    - Read stdin after parse_args so that --help works
    - Don't print 'parts'
    - Double decode by passing -d twice

--- libexec/philutils/urlencode.py
+++ libexec/philutils/urlencode.py
@@ -8,19 +8,19 @@ DESCRIPTION = """Command line interface to the urllib.parse.quote (encoding)
 and urllib.parse.unquote (decoding) functions in the python package urllib.
 
 Reads from stdin and prints the encoded or decoded URL"""
-
-url = sys.stdin.read().strip()
-
 p = argparse.ArgumentParser()
-p.add_argument("--decode", "-d", action='store_true')
+p.add_argument("--decode", "-d", action='count', help="Set to decoding mode.  Use -dd to decode query values a second time")
 p.add_argument("--split", action='store_true', help="Split the URL an print everything on separate lines")
 p.add_argument("--quote-slash", action='store_true')
 args = p.parse_args()
+print(args)
+
+url = sys.stdin.read().strip()
+
 
 if args.decode:
     if args.split:
         parts = urllib.parse.urlparse(url)
-        print(parts)
         query = parts.query.split('&')
         print(f"scheme: \033[4m{parts.scheme}\033[0m")
         print(f"netloc: \033[1;35m{parts.netloc}\033[0m")
@@ -31,6 +31,8 @@ if args.decode:
         if parts.query:
             for qp in query:
                 key, value = map(urllib.parse.unquote, qp.split('=', 1))
+                if args.decode > 1:
+                    value = urllib.parse.unquote(value)
                 print(f"    \033[1;33m{key}\033[0m=\033[32m{value}\033[0m")
         if parts.fragment:
             print(f"fragment: \033[34m{parts.fragment}\033[0m")
