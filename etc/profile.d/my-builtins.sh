cd99bcd14a570e0e8c5e7686402b6a211400dbef
Philippe Carphin <phil103@hotmail.com>
2025-11-19 16:13:40 -0500

    Only build my builtins if BASH_DEV_DIR is defined

--- etc/profile.d/my-builtins.sh
+++ etc/profile.d/my-builtins.sh
@@ -1,2 +1,8 @@
-philutils_root=$(cd -P $(dirname ${BASH_SOURCE[0]})/../../ && pwd)
-enable -f ${philutils_root}/libexec/philutils/my-builtins.so shell_split
+philutils_setup_my_builtins(){
+    local philutils_root=$(cd -P $(dirname ${BASH_SOURCE[0]})/../../ && pwd)
+    local philutils_my_builtins=${philutils_root}/libexec/philutils/my-builtins.so
+    if [[ -f ${philutils_my_builtins} ]] ; then
+        enable -f ${philutils_my_builtins} shell_split
+    fi
+}
+philutils_setup_my_builtins ; unset -f $_
