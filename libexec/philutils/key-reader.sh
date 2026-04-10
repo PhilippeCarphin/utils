229af0a6d18e1ca24c18f766fd046078524650fd
Philippe Carphin <phil103@hotmail.com>
2026-04-10 19:10:56 -0400

    WIP update key reader

--- libexec/philutils/key-reader.sh
+++ libexec/philutils/key-reader.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env -S bash -o errexit -o nounset -o errtrace -o pipefail -O inherit_errexit -O nullglob -O extglob
 
+stty intr ''
+stty quit ''
+stty kill ''
+stty eof ''
+stty start ''
+stty stop ''
+stty susp ''
+stty rprnt ''
+stty werase ''
+stty lnext ''
+stty discard ''
 
 while read -s -N 1 b ; do
     printf "(((%q)))\n" "${b}"
     case "$b" in
-        $'\020') printf "[C-p]-bing-bong\n" ;;
+        $'\003')
+            printf "C-c pressed: quitting\n" ; exit 130 ;;
         $'\e')
             read -r -s -t 0.1 -n 2 bing || true
             printf "<<<%q>>>\n" "${bing}"
