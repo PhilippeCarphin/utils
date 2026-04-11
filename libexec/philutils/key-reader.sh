a2b2c41149d1a2430c7dac3e873b79f8101e8dc6
Philippe Carphin <phil103@hotmail.com>
2026-04-11 09:57:29 -0400

    Update key-reader with stty and key names

--- libexec/philutils/key-reader.sh
+++ libexec/philutils/key-reader.sh
@@ -1,30 +1,70 @@
 #!/usr/bin/env -S bash -o errexit -o nounset -o errtrace -o pipefail -O inherit_errexit -O nullglob -O extglob
 
-stty intr ''
-stty quit ''
-stty kill ''
+saved_stty_settings=$(stty -g)
+trap "stty '${saved_stty_settings}'" EXIT
+
+if [[ $(uname) == Darwin ]] ; then
+    stty dsusp ''
+    stty reprint ''
+    stty status ''
+fi
+
+# I don't know what these do but they are in the cchars section
+# of stty report on MacOS
+stty min '0'
+stty time '0'
+
+stty discard ''
 stty eof ''
+stty erase ''
+stty intr ''
+stty kill ''
+stty lnext ''
+stty quit ''
+stty rprnt ''
 stty start ''
 stty stop ''
 stty susp ''
-stty rprnt ''
 stty werase ''
-stty lnext ''
-stty discard ''
 
+declare -gA key_map=(
+    # C-qwertyuiop
+    [$'\021']="C-q" [$'\027']="C-w" [$'\005']="C-e" [$'\022']="C-r"
+    [$'\024']="C-t" [$'\031']="C-y" [$'\025']="C-u" [$'\t']="C-i/TAB"
+    [$'\017']="C-o" [$'\020']="C-p"
+
+    # C-asdfghjkl
+    [$'\001']="C-a" [$'\023']="C-s" [$'\004']="C-d" [$'\006']="C-f"
+    [$'\a']="C-g" [$'\b']="C-h" [$'\v']="C-k"
+    [$'\f']="C-l"
+
+    # C-zxcvbnm,./
+    [$'\032']="C-z" [$'\030']="C-x" [$'\003']="C-c" [$'\026']="C-v"
+    [$'\002']="C-b" [$'\016']="C-n" [$'\n']="C-m/C-j/Enter" [$'\,']="," [$'\.']="."
+    [$'\037']="C-/"
+)
+
+printf "Waiting for key presses ...\n"
 while read -s -N 1 b ; do
-    printf "(((%q)))\n" "${b}"
     case "$b" in
         $'\003')
-            printf "C-c pressed: quitting\n" ; exit 130 ;;
+            read -p "C-c pressed: quit? y/n > " answer
+            if [[ ${answer} == y* ]] ; then kill -INT $$ ; fi ;;
         $'\e')
             read -r -s -t 0.1 -n 2 bing || true
             printf "<<<%q>>>\n" "${bing}"
             case "$bing" in
-                '[A') printf "up-arrow-bing-bong\n" ;;
-                '[B') printf "down-arrow-bing-bong\n" ;;
+                '[A') printf "up-arrow\n" ;;
+                '[B') printf "down-arrow\n" ;;
+                '[C') printf "right-arrow\n" ;;
+                '[D') printf "left-arrow\n" ;;
                 '') printf "escape-BING-BONG\n" ;;
             esac
             ;;
+        *)  if [[ -n "${key_map[$b]:-}" ]] ; then
+                printf "byte:%q (sent by pressing ${key_map[$b]})\n" "$b"
+            else
+                printf "byte:%q\n" "$b"
+            fi
     esac
 done
